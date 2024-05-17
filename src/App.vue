b762fce9c4cb2b46324c5f8d104d46cd8b5494f7
nobelsaputra53 <150402008+nobelsaputra@users.noreply.github.com>
2024-05-17 23:17:25 +0800

    testing V2

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,20 @@
 <template>
-  <div>
-    <h1>Counter App</h1>
-    <p>Jumlah: {{ count }}</p>
-    <CustomButton @click="tambah" />
-    <CustomButton @click="kurang" />
-  </div>
+  <Navbar />
 </template>
 
 <script>
 import { ref } from 'vue';
-import CustomButton from './components/Button.vue'; // Impor komponen CustomButton
+import CustomButton from './components/Button.vue';
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
-    CustomButton, // Daftarkan komponen CustomButton
+    CustomButton,
+    Navbar,
   },
   setup() {
-    const count = ref(0);
-
-    const tambah = () => {
-      count.value++;
-    };
-
-    const kurang = () => {
-      count.value--;
-    };
-
-    return {
-      count,
-      tambah,
-      kurang,
-    };
+    return {};
   },
 };
 </script>
