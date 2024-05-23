fd5d595e6759785597f393ebad23cc580196359d
nobelsaputra53 <150402008+nobelsaputra@users.noreply.github.com>
2024-05-23 20:31:32 +0800

    Update Home.vue

--- src/components/content/Home.vue
+++ src/components/content/Home.vue
@@ -2,7 +2,7 @@
   <section class="container mx-auto pt-36">
     <div class="flex flex-wrap md:flex-nowrap items-center">
       <div class="w-full md:w-1/2 p-6">
-        <h1 class="lg:text-4xl text-base font-bold text-gray-800 mb-2">Welcome to <span class="text-primary">JapaneseU</span></h1>
+        <h1 class="lg:text-4xl text-xl font-bold text-gray-800 mb-2">Welcome to <span class="text-primary">JapaneseU</span></h1>
         <p class="text-md text-gray-600 mb-6">If you like japan, you on the right side !</p>
         <dom></dom>
       </div>
